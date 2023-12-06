4a919d4cf82c9c9103ced79f8c8a75ebf536ac6d
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2023-12-06 15:33:20 +0200

    Add existing project files prior to the push to GitHub.

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by mohamed ramadan on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
